077f21d872817d3c032a0e391b689131114b9676
Ali Syed <alisyed0206@gmail.com>
2025-01-26 19:59:39 -0500

    added starter files and worked on implicit animations

---
+++ Animations/Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by Ali Syed on 2025-01-26.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Animations/AnimationsTests/AnimationsTests.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsTests.swift
+//  AnimationsTests
+//
+//  Created by Ali Syed on 2025-01-26.
+//
+
+import Testing
+@testable import Animations
+
+struct AnimationsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
