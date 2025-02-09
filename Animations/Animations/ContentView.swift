3ab4860d73cc188c90085d0483bc492081743c02
Ali Syed <alisyed0206@gmail.com>
2025-02-09 10:12:36 -0500

    animation gestures part 1, card with implicit vs explicit animations

--- Animations/Animations/ContentView.swift
+++ Animations/Animations/ContentView.swift
@@ -9,18 +9,26 @@ import SwiftUI
 
 struct ContentView: View {
     
-    @State private var enabled = false
+    @State private var dragAmount = CGSize.zero
     
     var body: some View {
-        Button("Tap Me") {
-            enabled.toggle()
-        }
-        .frame(width: 200, height: 200)
-        .background(enabled ? .blue : .red)
-        .animation(.default, value: enabled)
-        .foregroundStyle(.white)
-        .clipShape(.rect(cornerRadius: enabled ? 60 : 0))
-        .animation(.spring(duration: 1, bounce: 0.6), value: enabled)
+        LinearGradient(colors: [.yellow, .red], startPoint: .topLeading, endPoint: .bottomTrailing)
+            .frame(width: 300, height: 200)
+            .clipShape(.rect(cornerRadius: 10))
+            .offset(dragAmount)
+            .gesture(
+                DragGesture()
+                    .onChanged { dragAmount = $0.translation }
+                    //.onEnded { _ in dragAmount = .zero}
+                    .onEnded { _ in
+                        //explicit animation
+                        withAnimation(.bouncy) {
+                            dragAmount = .zero
+                        }
+                    }
+            )
+            //implicit animation
+            //.animation(.bouncy, value: dragAmount)
     }
 }
 
